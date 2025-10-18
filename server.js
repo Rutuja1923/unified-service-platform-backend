0fa05df99c79852a76380a5eb8b855071dca9d3e
Rutuja1923 <rutujajirage19@gmail.com>
2025-10-18 10:29:38 +0530

    set up cors

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
